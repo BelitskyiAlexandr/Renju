c4a0aeb893f9867bec2b90fadd1cb5abb3a74d0a
BelitskyiAlexandr <vilisapett@gmail.com>
2025-03-19 18:43:54 +0200

    init commit

---
+++ src/main/java/com/example/renju/RenjuApplication.java
@@ -0,0 +1,13 @@
+package com.example.renju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenjuApplication.class, args);
+	}
+
+}
